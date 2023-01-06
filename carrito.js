5c7de56573e2b027a5a23cc267fcf83abcbd0d96
LandaNahuel <landanahuel@icloud.com>
2023-01-05 21:30:13 -0300

    3erPreentrega

--- carrito.js
+++ carrito.js
@@ -1,5 +1,5 @@
-//Aca declaramos el listado de productos
-const productos = [
+
+const productos = [//Aca declaramos el listado de productos
     {id:1, cat: "Cafe", nombre:"Bonafide", desc:"Cafe en granos torrado suabe", pres:"paquete por 250gr", precio:639},
     {id:2, cat: "Cafe", nombre:"La Virgina", desc:"Cafe en granos torrado intenso", pres:"paquete por 500gr", precio:1129},
     {id:3, cat: "Cafe", nombre:"Morenita", desc:"Cafe en capsula", pres:"20 unidades", precio:429},
@@ -42,10 +42,10 @@ vaciarCarrito.addEventListener("click", () => { //declaramos un evento de tipo c
 })
 
 
-productos.forEach(productos => {
-    let div= document.createElement("div");
-    div.innerHTML = `
-    <h2>${productos.nombre}</h2>
+productos.forEach(productos => { //Usamos un forEach para que por cada producto...
+    let div= document.createElement("div");//...se cree un elemento div...
+    div.innerHTML = //...y que por dentro contenga los siguientes elementos
+    `<h2>${productos.nombre}</h2>
     <p>Categoria: ${productos.cat}</p>
     <p>Descripcion: ${productos.desc}</p>
     <p>Presentacion: ${productos.pres}</p>
@@ -53,39 +53,39 @@ productos.forEach(productos => {
     <button id=boton${productos.id}> Agregar </button>
     `;
 
-    contenedor.append(div);
+    contenedor.append(div);//esta linea inserta el div que creamos en contenedor del HTML
 
-    let boton = document.getElementById(`boton${productos.id}`)
-
-    boton.addEventListener("click", () => agregarCarrito (productos.id,));
+    let boton = document.getElementById(`boton${productos.id}`)//Aca llamamos al boton que se genera en cada producto creado
+    boton.addEventListener("click", () => agregarCarrito (productos.id,));//...le asignamos un evento tipo click que llama a una funcion
 })
 
-const agregarCarrito = (prodId) => {
-    const item = productos.find ((prod) => prod.id === prodId)
-    carrito.push(item)
-    actualizarCarrito()
-    localStorage.setItem("carrito", JSON.stringify (carrito));
+const agregarCarrito = (prodId) => {//cramos un funcion que...
+    const item = productos.find ((prod) => prod.id === prodId)//..tomando como parametro el ID del producto...
+    carrito.push(item)//...agregamos el producto...
+    actualizarCarrito()//...y llamamos la funcion que actualiza el estado del carrito
+    localStorage.setItem("carrito", JSON.stringify (carrito));//...a travez de esta linea enviamos el estado del carrito al local storage de una manera que lo reconozca como un array usando JSON
 }
-const eliminarDelCarrito= (prodId) =>{
-    const item = carrito.find ((prod) => prod.id === prodId)
-    const indice= carrito.indexOf (item)
-    carrito.splice(indice, 1)
-    actualizarCarrito()
+const eliminarDelCarrito= (prodId) =>{//cramos un funcion que...
+    const item = carrito.find ((prod) => prod.id === prodId)//..tomando como parametro el ID del producto...
+    let indice= carrito.indexOf (item)//...declaramos una variable que alvergue el index del producto tomado anteriomente...
+    carrito.splice(indice, 1)//...y le decimo que elimie un elemento apartir de ese index tomado, por lo cual elimina solo ese producto
+    actualizarCarrito()//...actualizamos el estado del carrito
 }
 
 
-const actualizarCarrito = () =>{
-    contenedorCarrito.innerHTML=""
+const actualizarCarrito = () =>{//declaramos la funcion que tanto se uso anteriormente...
+    contenedorCarrito.innerHTML=""//...creamos un inner vacio para que no se dupliquen los elementos en el carrito
 
-    carrito.forEach(prod => {
-        let div = document.createElement(`div`)
-        div.innerHTML =`
+    carrito.forEach(prod => {//...y por cada elemento que s encuentre en el carrito...
+        let div = document.createElement(`div`)//...creamos un div...
+        div.innerHTML =//..que contenga en su interior los siguientes elementos:
+        `
         <p>${prod.nombre}</p>
         <p>Precio: $${prod.precio}</p>
         <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
         <button onclick = "eliminarDelCarrito(${prod.id})">Eliminar🗑️</button>
         `
-        contenedorCarrito.appendChild(div)
+        contenedorCarrito.appendChild(div)//esta linea inserta el div que creamos en contenedor del HTML
     })
 
 }
